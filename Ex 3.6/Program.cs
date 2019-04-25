3bb363b7caafd10023e740024f422bd18759f944
filipgrdenWSEI <filip.grden@microsoft.wsei.edu.pl>
2019-04-25 08:35:06 +0200

    tablica dwuwymiarowa suma

--- Ex 3.6/Program.cs
+++ Ex 3.6/Program.cs
@@ -10,6 +10,25 @@ namespace Ex_3._6
     {
         static void Main(string[] args)
         {
+            int[][] tab =
+            {
+                new int[] {1,2},
+                new int[] {3, 4, 5, 6 },
+                new int[] {7, 8, 9}
+            };
+
+            int sum = 0;
+            for(int i = 0; i < tab.Length; ++i)
+            {
+                for(int j = 0; j < tab[i].Length; ++j)
+                {
+                    sum += tab[i][j];
+                }
+            }
+
+            Console.WriteLine(sum);
+            Console.ReadKey();
+
         }
     }
 }
